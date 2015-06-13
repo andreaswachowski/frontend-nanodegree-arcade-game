fd0ab751ca470b17887390fb302b08ca69f016be
Andreas Wachowski <andreas.wachowski@gmail.com>
2015-06-13 14:59:29 +0200

    docs: Add comment

--- js/app/tile.js
+++ js/app/tile.js
@@ -3,5 +3,9 @@
 function Tile() {
 }
 
+// Because the height- and width-properties are the same on all objects, I
+// wanted to model them as Java-like static class variables and therefore
+// attach them directly to Tile, as opposed to writing them inside the
+// constructor as this.height and this.width
 Tile.height = 83;
 Tile.width = 101;
